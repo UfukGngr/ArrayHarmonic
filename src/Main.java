8a71272a3bd306500f1de64ea4d21c835fe02469
UfukGngr <ufukgngr94@gmail.com>
2022-11-28 23:01:25 +0300

    www.patika.dev

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6};
+        int[] arr={1,2,2};
         double h=0;
         for (int i=0;i<arr.length;i++){
             h+=(1.0/arr[i]);
         }
+        h=arr.length/h;
         System.out.println(h);
 
 
